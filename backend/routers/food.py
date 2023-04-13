ddc5c00896fcccf8e469dad2a9332d59db10f827
Юрий Данилов <yuradan0903@gmail.com>
2023-04-13 18:09:54 +0300

    api food routers

---
+++ backend/routers/food.py
@@ -0,0 +1,38 @@
+import logging
+from fastapi import APIRouter, Body, Depends
+from typing import List
+from ..services.food import FoodService
+from ..schemas import food as food_sch
+
+router = APIRouter(prefix="/api/v1/food")
+logger = logging.getLogger(f'app.{__name__}')
+
+
+@router.get("")
+async def get_foods():
+    return await FoodService.get_foods()
+
+
+@router.post("", response_model=food_sch.FoodResponse)
+async def create_food(food_item: food_sch.FoodCreate):
+    return await FoodService.create_food(food_item)
+
+
+@router.get("/count")
+async def get_count_of_food():
+    return await FoodService.get_count_of_food()
+
+
+@router.get("/{food_id}", response_model=food_sch.FoodResponse)
+async def get_food_by_id(food_id: int):
+    return await FoodService.get_food_by_id(food_id)
+
+
+@router.put("/{food_id}")
+async def change_food_info(food_id: int, food_item: food_sch.FoodUpdate):
+    return await FoodService.change_food_info(food_id, food_item)
+
+
+@router.delete("/{food_id}", status_code=204)
+async def delete_food(food_id: int):
+    return await FoodService.delete_food(food_id)
