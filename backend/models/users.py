a3ad14f750c9e2420a98554f827cc3ed377ab75d
Korneev Yaroslav <agrimer2007@yandex.ru>
2023-04-19 02:19:24 +0300

    ~users model

--- backend/models/users.py
+++ backend/models/users.py
@@ -3,8 +3,8 @@ from sqlalchemy.dialects import postgresql as pg
 from ..settings.db import metadata
 
 
-customer = sql.Table(
-    "customer",
+users = sql.Table(
+    "users",
     metadata,
     sql.Column("user_id", sql.Integer, primary_key=True),
     sql.Column("username", sql.VARCHAR(50)),
