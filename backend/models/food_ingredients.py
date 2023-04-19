78f1d9e6d99f07c6a0c67ec2dd6e1b0ea7ec30f0
Юрий Данилов <yuradan0903@gmail.com>
2023-04-19 17:02:22 +0300

    ~Renamed

--- backend/models/food_ingredients.py
+++ backend/models/food_ingredients.py
@@ -2,9 +2,9 @@ import sqlalchemy as sql
 from ..settings.db import metadata
 import sqlalchemy.dialects.postgresql as pg
 
-food_ingridients = sql.Table(
-    "food_ingridients",
+food_ingredients = sql.Table(
+    "food_ingredients",
     metadata,
     sql.Column("food_fk", sql.Integer),
-    sql.Column("ingridient_fk", sql.Integer)
+    sql.Column("ingredient_fk", sql.Integer)
 )
--- backend/models/ingredients.py
+++ backend/models/ingredients.py
@@ -2,9 +2,9 @@ import sqlalchemy as sql
 from ..settings.db import metadata
 import sqlalchemy.dialects.postgresql as pg
 
-ingridients = sql.Table(
-    "ingridients",
+ingredients = sql.Table(
+    "ingredients",
     metadata,
-    sql.Column("ingridients_id", sql.Integer, primary_key=True),
+    sql.Column("ingredients_id", sql.Integer, primary_key=True),
     sql.Column("name", sql.VARCHAR(30))
 )
